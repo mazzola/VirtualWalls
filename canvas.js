d01dd2b58d4c2a90707034bdc7fe40c6406a39a6
Michael Mazzola <mazzola70@gmail.com>
2013-02-11 17:13:13 -0500

    Proof of concept. Draggable element and getting position of an element

---
+++ canvas.js
@@ -0,0 +1,33 @@
+$(document).ready( function() {
+	var stage = new Kinetic.Stage({
+		container: 'container',
+		width: 600,
+	    height: 600
+	});
+
+	var layer = new Kinetic.Layer();
+
+	var circleRadius = 20;
+	var initX = stage.getWidth() / 2 - circleRadius;
+	var initY = stage.getHeight() / 2 - circleRadius;
+
+	var circle = new Kinetic.Circle({
+		x: initX,
+		y: initY,
+		radius: circleRadius,
+		fill: 'red',
+		stroke: 'black',
+		strokeWidth: 2,
+		draggable: true
+	}) ;
+
+	layer.add(circle);
+	stage.add(layer);
+
+	// Set onclick listener for button
+	$('#location-button').click(function() {
+		console.log(circle.getPosition());
+		$('#location').text('x: ' + circle.getX() + ' y: ' + circle.getY());
+	});
+	
+});
